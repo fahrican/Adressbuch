ea30341943fae6b8b62332bdd0c102efb8f30641
Fahri Can <fahri-can@web.de>
2018-06-10 23:17:38 +0200

    Display first name and number in a list.

---
+++ app.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    const divHoldContacts = document.getElementById("addressbook");
+
+    //const tData = JSON.parse("address_book.json");
+
+    var contacts = [
+        {
+            "name": "Matthias",
+            "number": "067678967"
+        },
+        {
+            "name": "Marco",
+            "number": "066478967"
+        },
+        {
+            "name": "Dominik",
+            "number": "065678969897"
+        },
+        {
+            "name": "Steve",
+            "number": "065078967"
+        },
+        {
+            "name": "Markus",
+            "number": "068078967"
+        }
+    ];
+
+    function iterateThroughContacts() {
+
+        const fragment = document.createDocumentFragment();
+        for (let i = 0; i < contacts.length; i++) {
+
+            const name = document.createElement("P");
+            const number = document.createElement("P");
+
+            name.innerText = contacts[i].name;
+            name.className = "alert alert-dark";
+            number.innerText = contacts[i].number;
+            number.className = "alert alert-success";
+
+            fragment.appendChild(name);
+            fragment.appendChild(number);
+        }
+
+        divHoldContacts.appendChild(fragment);
+    }
+
+    iterateThroughContacts();
+});
